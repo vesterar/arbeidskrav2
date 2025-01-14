bba1e8bd6c1e013da20739203d09aa7922201955
arve <arve.vesteraas@hotmail.com>
2025-01-14 21:28:24 +0100

    added description

--- Arbeidskrav2.1.py
+++ Arbeidskrav2.1.py
@@ -1,6 +1,6 @@
+# Program som tar ditt fødselsår for å beregne din alder.
+
 born = int(input("Skriv inn hvilket år du er født: "))
 
-age = (2024 - born)
-print(f"Du er eller blir {age} år i 2024")
-
-
+age = (2025 - born)
+print(f"Du er eller blir {age} år i 2025")
